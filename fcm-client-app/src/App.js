3f67cdd753ba2db134a9b9ffc4b0a5cbea9ea9bb
pavan-nw <pavansdoc@gmail.com>
2018-02-22 11:13:28 +0530

    Clean up.

--- fcm-client-app/src/App.js
+++ fcm-client-app/src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import firbaseApp from './lib/firebase-config';
-import 'primereact/resources/themes/omega/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'font-awesome/css/font-awesome.css';
-import { Button } from 'primereact/components/button/Button';
-import { Message } from 'primereact/components/message/Message';
-import { Accordion, AccordionTab } from 'primereact/components/accordion/Accordion';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firbaseApp from "./lib/firebase-config";
+import "primereact/resources/themes/omega/theme.css";
+import "primereact/resources/primereact.min.css";
+import "font-awesome/css/font-awesome.css";
+import { Button } from "primereact/components/button/Button";
+import { Message } from "primereact/components/message/Message";
+import {
+  Accordion,
+  AccordionTab
+} from "primereact/components/accordion/Accordion";
 const messaging = firbaseApp.messaging();
 const accordionTabList = [];
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
-      status: 'info',
-      message: 'Token will be displayed here..!',
-      title: 'Notifications will appear here..!',
-      msgCount: 'Notification Count: 0',
+      status: "info",
+      message: "Token will be displayed here..!",
+      title: "Notifications will appear here..!",
+      msgCount: "Notification Count: 0",
       accordionTabs: []
-    }
-    this.getToken = this.getToken.bind(this);
+    };
     this.handleClick = this.handleClick.bind(this);
     this.handleBgCb = this.handleBgCb.bind(this);
   }
@@ -31,41 +32,46 @@ class App extends Component {
     console.log(payload);
     const payloadMsg = JSON.stringify(payload);
 
-    const accordionTab = <AccordionTab header={payload.notification.title}>
-      {payloadMsg}
+    const accordionTab =
+      <AccordionTab header={payload.notification.title}>
+        {payloadMsg}
       </AccordionTab>;
+accordionTabList.push(accordionTab);
 
-      accordionTabList.push(accordionTab);
-
-      this.setState(...this.state, { accordionTabs: accordionTabList, msgCount: `Notification Count: ${accordionTabList.length}` });
-
+    this.setState(...this.state, {
+      accordionTabs: accordionTabList,
+      msgCount: `Notification Count: ${accordionTabList.length}`
+    });
   }
 
-
   handleClick() {
-
     console.log(messaging);
-
-    // messaging.setBackgroundMessageHandler(this.handleBgCb);
-
     messaging.onMessage(this.handleBgCb);
-
-    messaging.requestPermission()
+    messaging
+      .requestPermission()
       .then(() => {
-        console.log('Notification permission granted.');
-        messaging.getToken()
+        console.log("Notification permission granted.");
+        messaging
+          .getToken()
           .then(currentToken => {
             if (currentToken) {
-              console.log('Token generated is ', currentToken);
-              this.setState(...this.state, { status: 'success',
-message: currentToken });
+              console.log("Token generated is ", currentToken);
+              this.setState(...this.state, {
+                status: "success",
+                message: currentToken
+              });
               // sendTokenToServer(currentToken);
               // updateUIForPushEnabled(currentToken);
             } else {
               // Show permission request.
-              console.log('No Instance ID token available. Request permission to generate one.');
-              this.setState(...this.state, { status: 'error',
-message: 'No Instance ID token available. Request permission to generate one.' });
+              console.log(
+                "No Instance ID token available. Request permission to generate one."
+              );
+              this.setState(...this.state, {
+                status: "error",
+                message:
+                  "No Instance ID token available. Request permission to generate one."
+              });
 
               // Show permission UI.
               // updateUIForPushPermissionRequired();
@@ -73,49 +79,45 @@ message: 'No Instance ID token available. Request permission to generate one.' }
             }
           })
           .catch(err => {
-            console.log('An error occurred while retrieving token. ', err);
-            this.setState(...this.state, { status: 'error',
-message: 'An error occurred while retrieving token.' });
+            console.log("An error occurred while retrieving token. ", err);
+            this.setState(...this.state, {
+              status: "error",
+              message: "An error occurred while retrieving token."
+            });
 
             // showToken('Error retrieving Instance ID token. ', err);
             // setTokenSentToServer(false);
           });
-
       })
       .catch(err => {
-        console.log('Unable to get permission to notify.', err);
+        console.log("Unable to get permission to notify.", err);
       });
   }
 
-  getToken() {
-
-  }
-
   render() {
-    const appName = "Welcome to Firebase Client App"
+    const appName = "Welcome to Firebase Client App";
     return (
       <div>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{appName}</h1>
-        </header>
-        <p className="App-intro">
-          To get refresh token, click on <code>Generate Token</code> and send this to app Server.
-        </p>
-        <div>
-          <Button label="Generate Token" onClick={this.handleClick} />
-        </div>;
-        <div>
-          <Message severity={this.state.status} text={this.state.message}></Message>
-        </div>;
-        <div>
-          <Message severity="warn" text={this.state.msgCount}></Message>
-        </div>;
-      </div>
-      <Accordion multiple>
-          {this.state.accordionTabs}
-          </Accordion>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">{appName}</h1>
+          </header>
+          <p className="App-intro">
+            To get refresh token, click on <code>Generate Token</code> and send
+            this to app Server.
+          </p>
+          <div>
+            <Button label="Generate Token" onClick={this.handleClick} />
+          </div>;
+          <div>
+            <Message severity={this.state.status} text={this.state.message} />
+          </div>;
+          <div>
+            <Message severity="warn" text={this.state.msgCount} />
+          </div>;
+        </div>
+        <Accordion multiple>{this.state.accordionTabs}</Accordion>
       </div>
     );
   }
--- fcm-client-app/src/lib/firebase-config.js
+++ fcm-client-app/src/lib/firebase-config.js
@@ -2,22 +2,22 @@ import * as firebase from "firebase";
 
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
-const config = {
-    apiKey: "AIzaSyDabUni1OH4AvbCyNb4DR1vaA-HL9aMD1Q",
-    authDomain: "fcm-client-e4a3f.firebaseapp.com",
-    databaseURL: "https://fcm-client-e4a3f.firebaseio.com",
-    projectId: "fcm-client-e4a3f",
-    storageBucket: "",
-    messagingSenderId: "715065746267"
-  };
+// const config = {
+//     apiKey: "AIzaSyDabUni1OH4AvbCyNb4DR1vaA-HL9aMD1Q",
+//     authDomain: "fcm-client-e4a3f.firebaseapp.com",
+//     databaseURL: "https://fcm-client-e4a3f.firebaseio.com",
+//     projectId: "fcm-client-e4a3f",
+//     storageBucket: "",
+//     messagingSenderId: "715065746267"
+//   };
 
-  const sgcmConfig = {
-    apiKey: "AIzaSyCbS5r-sLFrCI49A4QSEauuAFdU1EVzRH0",
-    authDomain: "sgcm-194206.firebaseapp.com",
-    databaseURL: "https://sgcm-194206.firebaseio.com",
-    projectId: "sgcm-194206",
-    storageBucket: "sgcm-194206.appspot.com",
-    messagingSenderId: "1027703990575"
-  };
+const sgcmConfig = {
+  apiKey: "AIzaSyCbS5r-sLFrCI49A4QSEauuAFdU1EVzRH0",
+  authDomain: "sgcm-194206.firebaseapp.com",
+  databaseURL: "https://sgcm-194206.firebaseio.com",
+  projectId: "sgcm-194206",
+  storageBucket: "sgcm-194206.appspot.com",
+  messagingSenderId: "1027703990575"
+};
 
 export default firebase.initializeApp(sgcmConfig);
